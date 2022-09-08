e0ed1a1bb25e32d576fea72f5dda4fdf0dc5c055
Leonardo Dvulatk <leoalves_2010@yahoo.com.br>
2022-09-08 11:04:09 -0300

    Criação do componente NewVideoForm para adição de novos vídeos

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import VideoList from "./ui/components/data-display/VideoList";
+import NewVideoForm from "./ui/components/inputs/NewVideoForm";
 
 function App() {
     return (
         <div className="App">
             <VideoList />
+            <NewVideoForm />
         </div>
     );
 }
--- app/src/ui/components/inputs/NewVideoForm.jsx
+++ app/src/ui/components/inputs/NewVideoForm.jsx
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function NewVideoForm(props){
-    
+export default function NewVideoForm(props) {
+    const [title, setTitle] = useState("");
+    const [duration, setDuration] = useState("");
+    const [cover, setCover] = useState("");
+    const [url, setUrl] = useState("");
+
+    function save() {
+        const newVideo = {
+            title,
+            duration,
+            cover,
+            url,
+        };
+        //save
+        reset();
+    }
+
+    function reset() {
+        setTitle("");
+        setDuration("");
+        setCover("");
+        setUrl("");
+    }
+
+    return (
+        <div className="form">
+            <label>Title:</label>
+            <input
+                type="text"
+                value={title}
+                onChange={(event) => setTitle(event.target.value)}
+            />
+
+            <label>Duration:</label>
+            <input
+                type="text"
+                value={duration}
+                onChange={(event) => setDuration(event.target.value)}
+            />
+
+            <label>Cover:</label>
+            <input
+                type="text"
+                value={cover}
+                onChange={(event) => setCover(event.target.value)}
+            />
+
+            <label>URL:</label>
+            <input
+                type="text"
+                value={url}
+                onChange={(event) => setUrl(event.target.value)}
+            />
+
+            <button onClick={save}>Save</button>
+        </div>
+    );
 }
